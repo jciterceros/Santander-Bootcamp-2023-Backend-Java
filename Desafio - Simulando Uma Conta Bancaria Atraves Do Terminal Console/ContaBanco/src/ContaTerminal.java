8ce90d0ee0ac2d0a4d5edffca84db96aed6f879d
jciterceros <jciterceros@yahoo.com>
2023-08-21 23:14:32 -0300

    Codigo Testado

---
+++ Desafio - Simulando Uma Conta Bancaria Atraves Do Terminal Console/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        /*
+         * TODO:
+         * - Importar a classe Scanner
+         * - Exibir as mensagens para o usuario
+         * - Ler os valores digitados pelo usuario
+         * - Calcular o saldo final e exibir o resultado
+         * 
+         * Estrutura das variaveis:
+         * - Numero da conta: int exemplo: 1021
+         * - Agencia: String exemplo: "067-8"
+         * - Nome Cliente: String exemplo: "MARIO"
+         * - Saldo: double exemplo: 237.48
+         * 
+         * ao final exibir os dados no seguinte formato:
+         * "Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque"
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite o numero da conta: ");
+            int numeroConta = scanner.nextInt();
+
+            System.out.println("Digite a agencia: ");
+            String agencia = scanner.next();
+
+            System.out.println("Digite o nome do cliente: ");
+            String nomeCliente = scanner.next();
+
+            System.out.println("Digite o saldo: ");
+            double saldo = scanner.nextFloat();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                    + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
+        } catch (Exception e) {
+            System.out.println("Erro ao ler os dados");
+        }
+        scanner.close();
+    }
+}

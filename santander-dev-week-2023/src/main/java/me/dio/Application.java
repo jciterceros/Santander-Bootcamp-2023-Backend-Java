b0eddbc323573e6527a61f77cf5cc4a603611996
jciterceros <jciterceros@yahoo.com>
2023-09-07 20:00:26 -0300

    add ExceptionHandler and testing Swagger

--- santander-dev-week-2023/src/main/java/me/dio/Application.java
+++ santander-dev-week-2023/src/main/java/me/dio/Application.java
@@ -6,6 +6,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	/*
+	* Dados para Testes
+		{
+		  "name": "Fernando",
+		  "account": {
+			"number": "00000000-0",
+			"agency": "0000",
+			"balance": 1324.64,
+			"limit": 1000.00
+		  },
+		  "card": {
+			"number": "xxxx xxxx xxxx 0000",
+			"limit": 1000.00
+		  },
+		  "features": [
+			{
+			  "icon": "URL",
+			  "description": "Descrição da feature"
+			}
+		  ],
+		  "news": [
+			{
+			  "icon": "URL",
+			  "description": "Descrição da Novidade"
+			}
+		  ]
+		}
+	* */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
